a7029c2c691f6bdf168d9d6e89a6d526160ba92f
Phillip Evans Montinola <phillipevans27@gmail.com>
2024-11-07 21:56:10 -0700

    Add files via upload

---
+++ Assignment2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Assignment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.btnEnter = new System.Windows.Forms.Button();
+            this.textValue = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(-2, 1);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series7.Legend = "Legend1";
+            series7.Name = "s1";
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(408, 368);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // btnEnter
+            // 
+            this.btnEnter.Location = new System.Drawing.Point(224, 415);
+            this.btnEnter.Name = "btnEnter";
+            this.btnEnter.Size = new System.Drawing.Size(75, 23);
+            this.btnEnter.TabIndex = 1;
+            this.btnEnter.Text = "Enter";
+            this.btnEnter.UseVisualStyleBackColor = true;
+            this.btnEnter.Click += new System.EventHandler(this.btnEnter_Click);
+            // 
+            // textValue
+            // 
+            this.textValue.Location = new System.Drawing.Point(118, 415);
+            this.textValue.Name = "textValue";
+            this.textValue.Size = new System.Drawing.Size(100, 22);
+            this.textValue.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(30, 419);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter Value: ";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(305, 416);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(82, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(408, 490);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textValue);
+            this.Controls.Add(this.btnEnter);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button btnEnter;
+        private System.Windows.Forms.TextBox textValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Assignment2/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Assignment2
+{
+    public partial class Form1 : Form
+    {
+
+        private Color[] colors = new Color[]
+        {
+            Color.Red, Color.Blue, Color.Green,
+            Color.Yellow, Color.Purple, Color.Orange,
+            Color.Cyan, Color.Brown, Color.Magenta
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChart();
+        }
+
+        private void InitializeChart()
+        {
+            chart1.Series.Clear();
+            chart1.Legends.Clear();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            var input = textValue.Text;
+
+            // Check if input is empty
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                MessageBox.Show("Please enter some values separated by commas.");
+                return;
+            }
+
+            var values = input.Split(',')
+                .Select(v => v.Trim())
+                .Select(v =>
+                {
+                    if (int.TryParse(v, out int result))
+                        return result;
+                    else
+                    {
+                        MessageBox.Show($"Invalid input: '{v}'. Only integers are allowed.");
+                        return (int?)null;
+                    }
+                })
+                .Where(v => v.HasValue)
+                .Select(v => v.Value)
+                .ToArray();
+
+            if (values.Length == 0)
+            {
+                MessageBox.Show("Please enter valid integer values.");
+                return;
+            }
+
+            DisplayPieChart(values);
+        }
+
+        private void DisplayPieChart(int[] values)
+        {
+            chart1.Series.Clear();
+
+            Series series = new Series
+            {
+                ChartType = SeriesChartType.Pie,
+                Name = "Values"
+            };
+
+            // Calculate the total
+            int total = values.Sum();
+
+            for (int i = 0; i < values.Length; i++)
+            {
+                series.Points.AddXY($"Slice {i + 1}", values[i]);
+                series.Points[i].Color = colors[i % colors.Length];
+
+                // Set the slice label to just the slice name
+                series.Points[i].Label = $"Slice {i + 1}";
+
+                // Add the legend entry with percentage
+                double percentage = total > 0 ? (values[i] / (double)total) * 100 : 0;
+                series.Points[i].LegendText = $"{percentage:F1}%"; // Set percentage in the legend
+            }
+
+            // Add the series to the chart
+            chart1.Series.Add(series);
+
+            // Add legend
+            chart1.Legends.Clear();
+            chart1.Legends.Add(new Legend("Legend"));
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            chart1.Series.Clear();
+            chart1.Legends.Clear();
+
+            // Clear the input field
+            textValue.Clear();
+        }
+    }
+}
